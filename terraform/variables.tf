f6699c88e67a71baaa8e2584090f58e018a753e8
anarodriguezj <112646656+anarodriguezj@users.noreply.github.com>
2026-04-27 11:53:20 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,22 +10,22 @@ variable "gcp-network" {
 
 variable "gcp-region" {
   description = "GCP region"
-  type        = string
+  type        = "eu-west4"
 }
 variable "gcp-zone" {
   description = "GCP zone"
-  type        = string
+  type        = "eu-west4-a"
 }
 variable "gcp-cluster-name" {
   description = "Cluster name"
-  type        = string
+  type        = "anarodriguezj-td2026"
 }
 
 variable "gcp-node-count" {
   description = "Number K8s Worker nodes"
-  type        = number
+  type        = 2
 }
 variable "gcp-node-size" {
   description = "Size K8s Worker nodes"
-  type        = string
+  type        = "e2.small"
 }
